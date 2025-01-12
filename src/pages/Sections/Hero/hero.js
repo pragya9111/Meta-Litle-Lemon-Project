e075d71038fda90fc8454beaa6e98a5cd351cf5b
Pragya Bisen <pragyabisen14@gmail.com>
2025-01-12 16:21:35 +0530

    feat: added hero section in home page

--- src/pages/Sections/Hero/hero.js
+++ src/pages/Sections/Hero/hero.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import './hero.css'
+import Button from "../../../components/Button/button"
+import Image from "../../../assets/hero image.png";
+import { useNavigate } from "react-router-dom";
 
-function Hero() {
-    return (
-        <section className='hero-container'>Hero</section>
-    )
+const Hero = () => {
+
+  const navigate = useNavigate();
+
+  const onClickReserve = () => {
+    navigate('/reservations');
+  }
+
+  return (
+    <div className="hero-background">
+      <div className="hero">
+        <h1 className="title">Little Lemon</h1>
+        <h2 className="subtitle">Chicago</h2>
+        <p>
+          We are a family owned Mediterranean
+          restaurant, focused on traditional
+          recipes served with a modern twist.
+          Little Lemon is where
+          modern flair meets cozy nostalgia, offering
+          bespoke cocktails and a premium wine selection.
+        </p>
+        <Button title={"Reserve a table"} onClick={onClickReserve} />
+      </div>
+
+      <img
+        className="image"
+        src={Image}
+        alt="Restarurant food"
+        height={200}
+        width={200}
+      />
+    </div>
+  )
 }
 
 export default Hero
